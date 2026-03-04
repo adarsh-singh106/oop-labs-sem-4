ec808f6dfdb5c8178ccce9472d38b6f1e5f54e6d
Adarsh Singh <a1d0arsh6@gmail.com>
2026-03-04 12:37:06 +0530

    num1 repetation fix

--- #Lab Practicals/Practical_03.java
+++ #Lab Practicals/Practical_03.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 
 public class Practical_03 {
     public static boolean solution(int num1, int num2, int num3) {
-        int N1 = num1 % 10, N2 = num1 % 10, N3 = num1 % 10;
+        int N1 = num1 % 10, N2 = num2 % 10, N3 = num3 % 10;
         if ((N1 == N2) || (N2 == N3) || (N1 == N3))
             return true;
         return false;
